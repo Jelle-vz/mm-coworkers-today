d63f41cb673e56707a353d46930e8e9802fa7fcd
Jelle Verzijden <jelle@webverzorgers.nl>
2017-01-24 09:58:15 +0100

    REFACTORED api url + token moved to config

--- mm-coworkers-today.js
+++ mm-coworkers-today.js
@@ -15,43 +15,14 @@ Module.register("mm-coworkers-today",{
 
 	// Default module config.
 	defaults: {
-		apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
 		data:[
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"9-158js00RcqehtTVZPXWCrCINfdMSKNHDKRWJPCIS", //Jelle Verzijden
-				userId:"9"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"6-lrlUSZZ6rVMLuBXuqDNxFBpq0H3h3esjqb3q2Qlh", //Chris Bogaards
-				userId:"6"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"2-x7MtOIbEMHLGjLdFFCPs7npCAdhf5HHqDjKgRLKt", //Jordi Verzijden
-				userId:"2"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"17-vsPjvGmHbuS65jNBO7kp3CYDNH138g3szHCUcPre", //Menno Boer
-				userId:"17"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"103-6u1kEoufHJR1oHQXfgB9Fr8AncsGhJACzL7N7bNC", //Carlo Carels
-				userId:"103"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"13-69a8H2SfNXcWgukJ7nn3nyz8RtwWwrUwnrn7qtJc", //John  Verzijden
-				userId:"13"
-			},
-			{
-				apiUrl:"http://mijn.wvzonline.nl/public/api.php?",
-				apiToken:"66-aAdefGT7TsuVojq5e2nKKciXGnGYfrlgSpkXV4SD", //Ferry Brak
-				userId:"66"
-			}
+			{userId:"9"},
+			{userId:"6"},
+			{userId:"2"},
+			{userId:"17"},
+			{userId:"103"},
+			{userId:"13"},
+			{userId:"66"}
 		],
 		getJSON:function(url) {
 			let Httpreq = new XMLHttpRequest();
@@ -66,13 +37,11 @@ Module.register("mm-coworkers-today",{
 			return `Something whent wrong with fetching ${url}`;
 		},
 	},
-
 	getStyles: function(){
 		return [
 			'mm-coworkers-today.css'
 		]
 	},
-
 	start: function(){
 		//Do something when module starts but dom not loaded
 	},
@@ -81,13 +50,14 @@ Module.register("mm-coworkers-today",{
 	getDom: function() {
 
 		let wrapper = document.createElement("div");
-		wrapper.className = "small bright";
+			wrapper.className = "small bright";
 
 		let apiUrl = this.config.apiUrl;
+		let apiToken = this.config.apiToken;
 		let getJSON = url => this.config.getJSON(url);
 
 		let fetchedUsers = this.config.data.map((item) => {
-			let user = item.userId ? getJSON(apiUrl + "&auth_api_token=" + item.apiToken + "&path_info=people/1/users/" + item.userId +"&format=json") : "No user data specified";
+			let user = item.userId ? getJSON(apiUrl + "&auth_api_token=" + apiToken + "&path_info=people/1/users/" + item.userId +"&format=json") : "No user data specified";
 			return {
 				name: user.short_display_name,
 				avatar: user.avatar.photo,
